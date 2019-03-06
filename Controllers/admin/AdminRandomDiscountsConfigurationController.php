6710ba0c91a09cc29704e6801a8e380cb1f460e5
unknown <0wn3das@gmail.com>
2019-03-06 11:48:33 +0200

    Admin Controller forms

--- Controllers/admin/AdminRandomDiscountsConfigurationController.php
+++ Controllers/admin/AdminRandomDiscountsConfigurationController.php
@@ -8,5 +8,51 @@
 
 class AdminRandomDiscountsConfigurationController extends ModuleAdminController
 {
+    public function renderOptions()
+    {
+        $this->fields_form = array(
+            'legend' => array(
+                'title' => $this->l('Settings'),
+                'icon' => 'icon-edit',
+            ),
+            'input' => array(
+                    array(
+                    'type' => 'select',
+                    'label' => 'Kategorijų kiekis:',
+                    'name' => 'type_select',
+                    'options' => array(
+                        'query' => Zone::getZones(),
+                        'id' => 'id_zone',
+                        'name' => 'name',
+                        ),
+                    ),
+                    array(
+                        'type' => 'select',
+                        'label' => 'Prekių kiekis(per Kategorija)',
+                        'name' => 'type_select',
+                        'options' => array(
+                            'query' => Zone::getZones(),
+                            'id' => 'id_zone',
+                            'name' => 'name',
+                        ),
+                    ),
+                array(
+                    'type' => 'select',
+                    'label' => 'Nuolaida (%)',
+                    'name' => 'type_select',
+                    'options' => array(
+                        'query' => Zone::getZones(),
+                        'id' => 'id_zone',
+                        'name' => 'name',
+                    ),
+                ),
+            ),
+            'submit' => array(
+                'title' => 'Save',
+            ),
+//            'buttons' => array(),
+        );
 
+        return parent::renderForm();
+    }
 }
